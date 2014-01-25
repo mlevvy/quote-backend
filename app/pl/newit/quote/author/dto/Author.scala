1ac002dc34302b0402b70c0c927864600d6d96f2
Michał Kiędyś <michal@kiedys.net>
2014-01-26 00:30:59 +0100

    Change formatting and import ordering

--- app/pl/newit/quote/author/dto/Author.scala
+++ app/pl/newit/quote/author/dto/Author.scala
@@ -1,9 +1,12 @@
 package pl.newit.quote.author.dto
-
-import play.api.libs.json._
 import pl.newit.quote.common.Audit
 
-final case class Author(id: String, displayName: String, audit: Audit)
+import play.api.libs.json._
+
+final case class Author(
+  id: String,
+  displayName: String,
+  audit: Audit)
 
 object Author {
   implicit val format = Json.format[Author]
