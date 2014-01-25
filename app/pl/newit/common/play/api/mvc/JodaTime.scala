b29a4387a12079a598affc6978d492bb9014aed4
Michał Kiędyś <michal@kiedys.net>
2014-01-25 23:49:42 +0100

    Switch to async version of Result

--- app/pl/newit/common/play/api/mvc/JodaTime.scala
+++ app/pl/newit/common/play/api/mvc/JodaTime.scala
@@ -1,15 +1,17 @@
 package pl.newit.common.play.api.mvc
+import scala.concurrent.Future
+import scala.concurrent.Future.successful
 import scala.util.control.Exception.catching
 
 import org.joda.time.DateTime
 
-import play.api.mvc.Result
 import play.api.mvc.Results.BadRequest
+import play.api.mvc.SimpleResult
 
 trait JodaTime {
-  def datetime(from: String)(block: DateTime => Result): Result =
+  def datetime(from: String)(block: DateTime => Future[SimpleResult]): Future[SimpleResult] =
     catching(classOf[IllegalArgumentException])
       .opt(new DateTime(from))
       .map(block)
-      .getOrElse(BadRequest)
+      .getOrElse(successful(BadRequest))
 }
