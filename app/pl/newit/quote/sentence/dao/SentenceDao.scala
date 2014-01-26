503e3727fe13321267cf428f2c9252025f734585
Michał Kiędyś <michal@kiedys.net>
2014-01-26 18:31:08 +0100

    Add method for creating sentences to DAO

--- app/pl/newit/quote/sentence/dao/SentenceDao.scala
+++ app/pl/newit/quote/sentence/dao/SentenceDao.scala
@@ -1,9 +1,14 @@
 package pl.newit.quote.sentence.dao
+import scala.concurrent.Future
+
 import org.joda.time.Interval
 
 import pl.newit.quote.sentence.dto.Sentence
+import pl.newit.quote.sentence.dto.SentenceInput
 import play.api.libs.iteratee.Enumerator
 
 trait SentenceDao {
+  def create(from: SentenceInput, creatorId: String): Future[Sentence]
+
   def getAll(interval: Interval): Enumerator[Sentence]
 }
---
+++ app/pl/newit/quote/sentence/dto/SentenceInput.scala
@@ -0,0 +1,5 @@
+package pl.newit.quote.sentence.dto
+
+import org.joda.time.DateTime
+
+final case class SentenceInput(forDay: DateTime, content: String, authorId: String)
