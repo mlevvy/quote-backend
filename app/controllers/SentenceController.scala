3090883900f8a9219b4f73fd48edfe81589aef4c
Michał Kiędyś <michal@kiedys.net>
2014-02-08 22:23:12 +0100

    Require authentication in SentenceController

--- app/controllers/SentenceController.scala
+++ app/controllers/SentenceController.scala
@@ -1,17 +1,25 @@
 package controllers
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future.successful
 
 import com.google.inject.Inject
 
 import pl.newit.common.play.api.mvc.Collections
 import pl.newit.common.play.api.mvc.JodaTime
+import pl.newit.quote.auth.Authenticated
 import pl.newit.quote.service.SentenceService
-import play.api.mvc.Action
+import pl.newit.quote.service.dto.SentenceInput
 import play.api.mvc.Controller
 
 final class SentenceController @Inject() (service: SentenceService) extends Controller with JodaTime with Collections {
+  def create(authorId: String) =
+    Authenticated.async(parse.json)(request =>
+      request.body.validate[SentenceInput]
+        .map(input => successful(NotImplemented))
+        .recoverTotal(_ => successful(BadRequest)))
+
   def getAll(from: String) =
-    Action.async(
+    Authenticated.async(
       datetime(from)(
         service.getAll(_)
           .map(okOrNoContent(_))))
